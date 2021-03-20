d71409b2896234b36c180f73320297ca2c530fb1
artem_nanavov <artemnanavov@gmail.com>
2021-03-20 14:03:34 +0300

    init

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Link } from "react-router-native";
+import Home from './src/components/home/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeRouter>
+      <Link to="/login" underlayColor="#f0f4f7" style={{marginTop: 100}}>
+        <Text>Home</Text>
+      </Link>
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
+      <Route exact path="/" render={() => <Text>sdfsfsd</Text>} />
+      <Route exact path="/login" component={Home} />
+    </NativeRouter>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
